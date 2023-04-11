97fd46902fbe929bd6a7fda8216370c0f11e1c70
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2023-04-11 14:53:42 -0500

    Add PUT endpoint for updating an entire villa

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -89,4 +89,22 @@ public class VillaAPIController : ControllerBase
 
         return NoContent();
     }
+
+    [HttpPut("{id:int}", Name = "UpdateVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+    {
+        if (villaDTO == null || id != villaDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        villa.Name = villaDTO.Name;
+        villa.Occupancy = villaDTO.Occupancy;
+        villa.Sqft = villaDTO.Sqft;
+
+        return NoContent();
+    }
 }
