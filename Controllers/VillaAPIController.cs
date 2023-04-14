0e175935a3e4e2af1a2694952aceaf253cfa10df
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2023-04-14 10:11:59 -0500

    Add PATCH endpoint

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -1,5 +1,6 @@
 using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
@@ -104,6 +105,33 @@ public class VillaAPIController : ControllerBase
         villa.Name = villaDTO.Name;
         villa.Occupancy = villaDTO.Occupancy;
         villa.Sqft = villaDTO.Sqft;
+
+        return NoContent();
+    }
+
+    [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+    {
+        if (patchDTO == null || id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        if (villa == null)
+        {
+            return BadRequest();
+        }
+
+        patchDTO.ApplyTo(villa, ModelState);
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
 
         return NoContent();
     }
